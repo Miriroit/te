180fd7fa39a53ecb2434840bb00b13d89ee40ed1
MiriR <miriroid@gmail.com>
2017-08-16 15:27:50 +0300

    2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
 
-            List<int> resList = new List<int>(new int[] { 1, 2, 8, 1, 2, 3, 5, 6, 7, 2, 2, 9, 9, 9, 3, 2, 1, 2, 35 });
+            List<int> resList = new List<int>(new int[] { 1, 2, 8, 1, 2, 3, 5, 6, 7, 2, 2, 9, 9, 9, 3, 2, 1, 2, 35,100 });
             Logic l = new Logic(resList);
 
             int res = l.getNewPostion(3);
